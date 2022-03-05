2b25206050536b0dbeacef50735226d17d508578
Adrian Caceres <manuel.caceres@ort.edu.ar>
2022-03-05 10:47:21 -0300

    Comienzo cursada

---
+++ clase 0/ejemplo/index.js
@@ -0,0 +1,74 @@
+/* console.log('hola mundo')
+
+const j = 3
+
+console.log(j)
+
+j = 'mensaje'
+
+console.log(j) */
+
+// console.log('test')
+
+/* const lista = [3,4,5]
+
+
+console.log(lista)
+
+lista = 4
+
+console.log(lista)
+ */
+
+/* const lista = [3,4,'texto',false]
+console.log(lista)
+
+lista[1] = 4000
+console.log(lista)
+
+lista[1] = [5,6,7]
+console.log(lista)
+
+const otralista = lista
+console.log(otralista)
+otralista[0] = 'algo'
+
+console.log(otralista)
+console.log(lista)
+ */
+
+/* 
+var texto = 'text'
+texto[0] = 's'
+console.log(texto)
+
+ */
+
+/* res = true
+if (res ) {
+    console.log('verdadero')
+}
+
+var num = 4
+while (num < 10) {
+    console.log(num)
+    num++    
+}
+ */
+
+/* const palabra = 'algun texto'
+for(var letra of palabra){
+   console.log(letra)
+}
+ */
+
+const util = require('./utiles.js')
+
+console.log(util.suma(7,3))
+
+
+
+
+
+
+
---
+++ clase 0/ejemplo/utiles.js
@@ -0,0 +1,11 @@
+function suma(a,b) {
+    return a+b
+}
+
+function sumadeotrotipo() {
+
+}
+
+module.exports = {
+    suma
+}
