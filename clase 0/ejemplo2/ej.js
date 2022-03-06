83da5743530bd79cac01cb3a397cff617a5d9011
Adrian Caceres <manuel.caceres@ort.edu.ar>
2022-03-06 13:04:03 -0300

    otro

---
+++ clase 0/ejemplo2/ej.js
@@ -0,0 +1,10 @@
+console.log('algo')
+
+console.log("Escribe tu nombre");
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    console.log("Tu nombre es: " + 
+        d.toString().trim());
+  });
+
